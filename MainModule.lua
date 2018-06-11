3243c10e2ef7c235b70b4450e1f6fcfd517dec46
Nabfam <34952301+CriiStudio@users.noreply.github.com>
2018-06-11 14:09:47 -0700

    Update MainModule.lua
    Added core functions; as well as a find function on getqueues() to prevent adding a queue twice.

--- MainModule.lua
+++ MainModule.lua
@@ -18,8 +18,11 @@
 
 ]]
 
+local r = {}
 local q = {}
 local robins = {}
+local hosttime
+local lasthost
 
 local core = require(QueueModule)
 local rs = game:GetService("ReplicatedStorage")
@@ -32,24 +35,56 @@ function setup()
 	plrinqueue.Name = "plrInQueue"
 	plrinqueue.Parent = rs
 	end
+setup()
 
-function getrobins() -- HIGHLY SUGGESTED to run **SERVER SIDE**, may crash clients **IF IN WORKSPACE**
+
+
+function r.newrobin(robin)
+	if robin.Name == "ROBIN_SETUP" then
+		table.insert(robins, #robins + 1, robin)
+		end
+	end
+
+function add(plr)
+	core.add(plr)
+	end
+
+function leave(plr)
+	core.remove(plr)
+	end
+game.Players.PlayerLeaving:connect(leave)
+
+function r.getrobins() -- HIGHLY SUGGESTED to run **SERVER SIDE**, may crash clients **IF IN WORKSPACE**
   local objects = game.Workspace:GetDescendants() -- Might be getchildren, and getdescendants is nil? Don't know. :p
 	print("RUNNING ROBIN SCAN, LAG MAY OCCOUR")
   for i = 1, #objects do
     if objects[i].ClassName == "InstanceValue" then -- Might be ObJectValue. I'm on a school chromebook; can't check wiki. :(
       if objects[i].Parent.Name == "ROBIN_SETUP" then
-        table.insert(robins,#robins + 1,objects[i].Parent)
+				if core.find(objects[i],"bool") == false then
+        				table.insert(robins,#robins + 1,objects[i].Parent)
+					objects[i].JoinButton.MouseClick:connect(add)
+					objects[i].LeaveButton.MouseClick:connect(leave)
 				end
 			end
 		end
 	print("ROBIN SCAN FINISHED.")
 	end
 
-function newrobin(robin)
-	if robin.Name == "ROBIN_SETUP" then
-		table.insert(robins, #robins + 1, robin)
+function r.update()
+	q = core:getqueue()
+	if q[1] ~= lasthost then
+		hosttime = game.Workspace.DistributedGameTime
+		lasthost = q[1]
+		end
+	for i = 1, #robins do
+		robins[i].Host.Value = q[1]
+		robins[i].NextHost.Value = q[2]
+		robins[i].QueueConcat = table.concat(queue,', ')
+		robins[i].HostTimer = game.Workspace.DistributedGameTime - hosttime
 		end
 	end
+	
+	return r
+
 
 	
